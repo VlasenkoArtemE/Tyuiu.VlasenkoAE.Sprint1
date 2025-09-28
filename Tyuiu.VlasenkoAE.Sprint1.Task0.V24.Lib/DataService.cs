2446b536940683a0e81adda899f633efe545b86a
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-09-28 18:06:26 +0500

    Task0

--- Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Lib/DataService.cs
+++ Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Lib/DataService.cs
@@ -1,7 +1,12 @@
-﻿namespace Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
 
+namespace Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Lib
+{
+    public class DataService : ISprint1Task0V24
+    {
+        public double Calculate()
+        {
+            return (2 * 4 / 4 / 2 + 1);
+        }
     }
 }
--- Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Test/DataServiceTest.cs
+++ Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Test/DataServiceTest.cs
@@ -1,11 +1,16 @@
-﻿namespace Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Test
+﻿using Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Lib;
+
+namespace Tyuiu.VlasenkoAE.Sprint1.Task0.V24.Test
 {
     [TestClass]
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidExpression()
         {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
         }
     }
 }

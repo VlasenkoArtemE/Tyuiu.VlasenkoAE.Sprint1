0e85c0a1f053582ebac4d07a4738d90440536d66
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-09-29 22:33:01 +0500

    Task2

---
+++ Tyuiu.VlasenkoAE.Sprint1.Task2.V25.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.VlasenkoAE.Sprint1.Task2.V25.Lib;
+
+namespace Tyuiu.VlasenkoAE.Sprint1.Task2.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.ConvertRadsToDegrees(x);
+            Assert.AreEqual(114.592, res);
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint1.Task2.V25/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.VlasenkoAE.Sprint1.Task2.V25.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #25                                                             *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя                  *");
+Console.WriteLine("* исходные данные, выполняет указанные расчёты и печатает результат       *");
+Console.WriteLine("* на экране.                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x;
+
+Console.WriteLine("Введите значение угла в радианах:");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Угол в градусах = " + ds.ConvertRadsToDegrees(x));
+
+Console.ReadLine();

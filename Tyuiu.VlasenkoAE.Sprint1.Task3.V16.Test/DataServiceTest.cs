951489fb72460c92dc29f8fa09fbd35e4b4b73f8
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-09-30 20:35:56 +0500

    Task3

---
+++ Tyuiu.VlasenkoAE.Sprint1.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+using Tyuiu.VlasenkoAE.Sprint1.Task3.V16.Lib;
+
+namespace Tyuiu.VlasenkoAE.Sprint1.Task3.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 2;
+            double x2 = 3;
+            double wait = -5;
+            var res = ds.CoeffOfQuadraticEquation(x1, x2);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint1.Task3.V16/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.VlasenkoAE.Sprint1.Task3.V16.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #16                                                             *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет и печатает коэффициент            *");
+Console.WriteLine("* приведенного квадратного уравнения, корнями которого являются           *");
+Console.WriteLine("* введенные пользователем два вещественных числа.                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+double x1;
+double x2;
+
+Console.WriteLine("Введите значение X1:");
+x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение X2:");
+x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Коэффициент приведенного квадратного уравнения = " + ds.CoeffOfQuadraticEquation(x1, x2));
+
+Console.ReadLine();
